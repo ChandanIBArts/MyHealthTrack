dad188411c0f83265f9e3438d9b8c51b49d66797
Chandan <chandanmondal@ibarts.in>
2024-03-20 17:53:04 +0530

    commit all

---
+++ MyHealthTrack/UserInterface/Screens/Summary/TVCell/SummaryTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  SummaryTVCell.swift
+//  MyHealthTrack
+//
+//  Created by IB Arts Mac on 20/03/24.
+//
+
+import UIKit
+
+class SummaryTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
