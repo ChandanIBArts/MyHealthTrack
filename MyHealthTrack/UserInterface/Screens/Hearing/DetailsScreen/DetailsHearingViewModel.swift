7c307abb2bf740f9c78df99b691cfea268fa7261
Chandan <chandanmondal@ibarts.in>
2024-03-27 17:30:18 +0530

    commit all

---
+++ MyHealthTrack/UserInterface/Screens/Hearing/DetailsScreen/DetailsHearingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsHearingViewModel.swift
+//  MyHealthTrack
+//
+//  Created by IB Arts Mac on 27/03/24.
+//
+
+import Foundation
---
+++ MyHealthTrack/UserInterface/Screens/Sleep/SleepViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SleepViewModel.swift
+//  MyHealthTrack
+//
+//  Created by IB Arts Mac on 27/03/24.
+//
+
+import Foundation
